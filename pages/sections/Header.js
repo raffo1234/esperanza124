825d1b632866f4b3e66d4e138bfd1a41f939af99
Rafael Meza <rhmcord@gmail.com>
2022-08-14 16:13:46 -0500

    Add shadow to header

--- pages/sections/Header.js
+++ pages/sections/Header.js
@@ -5,7 +5,7 @@ import Container from "components/Container";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-30 bg-white">
+    <header className="fixed top-0 left-0 right-0 z-30 bg-white shadow-2xl">
       <Container>
         <div className="flex items-center justify-between py-4">
           <h1>
