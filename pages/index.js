bc912b68bf11aeeea595088dcd4d527c950aa5c0
Rafael Meza <rhmcord@gmail.com>
2023-01-22 11:32:07 -0500

    feat: add next-seo

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,35 @@ import Features from "./sections/Features";
 import Gallery from "./sections/Gallery";
 import Address from "./sections/Address";
 import Footer from "./sections/Footer";
+import { NextSeo } from "next-seo";
 
 const sections = [Hero, Features, Gallery, Address, Footer];
 
 export default function Home() {
   return (
-    <div className="text-sm bg-capri lg:text-base">
-      <Head>
-        <title>Esperanza 124</title>
-        <meta name="description" content="Proyecto | Esperanza 124" />
-      </Head>
-      <Header />
-      {sections.map((Section, index) => (
-        <Section key={index} />
-      ))}
-    </div>
+    <>
+      <NextSeo
+        title="Esperanza 124"
+        description="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
+        canonical="https://esperanza124.com/"
+        openGraph={{
+          title: "Esperanza 124",
+          description:
+            "Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata",
+          url: "https://esperanza124.com/",
+          site_name: "Esperanza 124",
+        }}
+      />
+      <div className="text-sm bg-capri lg:text-base">
+        <Head>
+          <title>Esperanza 124</title>
+          <meta name="description" content="Esperanza 124" />
+        </Head>
+        <Header />
+        {sections.map((Section, index) => (
+          <Section key={index} />
+        ))}
+      </div>
+    </>
   );
 }
