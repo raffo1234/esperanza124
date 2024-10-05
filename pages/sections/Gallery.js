431d42d01cb4bf9fbf6f0f45b60af81192a85c7b
Rafael Meza <rhmcord@gmail.com>
2024-10-05 17:38:44 -0500

    feat: add navigation functionality to lightbox

--- pages/sections/Gallery.js
+++ pages/sections/Gallery.js
@@ -12,7 +12,6 @@ import kitchen from "public/images/1.jpg";
 import livingRoom from "public/images/2.jpg";
 import bathroom from "public/images/3.jpg";
 import bathroom2 from "public/images/4.jpg";
-import stairs from "public/images/1.jpg";
 import { Fade } from "react-reveal";
 import Section from "components/Section";
 
@@ -29,10 +28,70 @@ const useEscape = (onEscape) => {
   }, [onEscape]);
 };
 
+const usePrev = (onPrev) => {
+  useEffect(() => {
+    const handlePrev = (event) => {
+      if (event.keyCode === 37) onPrev();
+    };
+    window.addEventListener("keydown", handlePrev);
+
+    return () => {
+      window.removeEventListener("keydown", handlePrev);
+    };
+  }, [onPrev]);
+};
+
+const useNext = (onNext) => {
+  useEffect(() => {
+    const handleNext = (event) => {
+      if (event.keyCode === 39) onNext();
+    };
+    window.addEventListener("keydown", handleNext);
+
+    return () => {
+      window.removeEventListener("keydown", handleNext);
+    };
+  }, [onNext]);
+};
+
 export default function Gallery() {
+  const images = [
+    image2,
+    image6,
+    image3,
+    image4,
+    image5,
+    kitchen,
+    livingRoom,
+    bathroom,
+    bathroom2,
+  ];
+
   const [lightboxDisplay, setLightboxDisplay] = useState(false);
-  const [imageToShow, setImageToShow] = useState(null);
+  const [imageToShow, setImageToShow] = useState(0);
+  let currentIndex = images.indexOf(imageToShow);
+
+  const showPrev = () => {
+    if (currentIndex <= 0) {
+      setImageToShow(images[images.length - 1]);
+    } else {
+      let prevImage = images[currentIndex - 1];
+      setImageToShow(prevImage);
+    }
+  };
+
+  const showNext = () => {
+    if (currentIndex >= images.length - 1) {
+      setImageToShow(images[0]);
+    } else {
+      let nextImage = images[currentIndex + 1];
+      setImageToShow(nextImage);
+    }
+  };
+
   useEscape(() => setLightboxDisplay(false));
+  usePrev(() => showPrev());
+  useNext(() => showNext());
 
   return (
     <>
@@ -45,12 +104,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(image2);
+                    setImageToShow(images[0]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={image2}
+                    src={images[0]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -67,12 +126,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(image6);
+                    setImageToShow(images[1]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={image6}
+                    src={images[1]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -85,12 +144,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(image3);
+                    setImageToShow(images[2]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={image3}
+                    src={images[2]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -107,12 +166,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(image4);
+                    setImageToShow(images[3]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={image4}
+                    src={images[3]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -129,12 +188,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(image2);
+                    setImageToShow(images[4]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={image2}
+                    src={images[4]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -147,12 +206,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(image5);
+                    setImageToShow(images[5]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={image5}
+                    src={images[5]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -169,12 +228,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(image3);
+                    setImageToShow(images[6]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={image3}
+                    src={images[6]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -191,12 +250,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(kitchen);
+                    setImageToShow(images[7]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={kitchen}
+                    src={images[7]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -209,74 +268,12 @@ export default function Gallery() {
                 <button
                   onClick={() => {
                     setLightboxDisplay(true);
-                    setImageToShow(livingRoom);
+                    setImageToShow(images[8]);
                   }}
                   className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
                 >
                   <Image
-                    src={livingRoom}
-                    className="relative z-10"
-                    alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
-                    title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                    quality={100}
-                  />
-                </button>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="flex items-center space-x-3">
-                <button
-                  onClick={() => {
-                    setLightboxDisplay(true);
-                    setImageToShow(stairs);
-                  }}
-                  className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
-                >
-                  <Image
-                    src={stairs}
-                    className="relative z-10"
-                    alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
-                    title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                    quality={100}
-                  />
-                </button>
-              </div>
-            </Fade>
-            <Fade>
-              <div className="items-center space-y-3 lg:space-y-0 lg:space-x-3 lg:flex">
-                <button
-                  onClick={() => {
-                    setLightboxDisplay(true);
-                    setImageToShow(bathroom);
-                  }}
-                  className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
-                >
-                  <Image
-                    src={bathroom}
-                    className="relative z-10"
-                    alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
-                    title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                    quality={100}
-                  />
-                </button>
-                <button
-                  onClick={() => {
-                    setLightboxDisplay(true);
-                    setImageToShow(bathroom2);
-                  }}
-                  className="relative flex-1 h-[350px] lg:h-[500px] overflow-hidden rounded-sm"
-                >
-                  <Image
-                    src={bathroom2}
+                    src={images[8]}
                     className="relative z-10"
                     alt="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
                     title="Esperanza 124, Departamentos en Venta en Huancayo El Tambo Pio Pata"
@@ -292,7 +289,7 @@ export default function Gallery() {
         </Container>
       </Section>
       {lightboxDisplay ? (
-        <section className="fixed top-0 left-0 z-40 w-full h-full bg-black bg-opacity-50 ">
+        <section className="fixed top-0 left-0 z-40 w-full h-full bg-white">
           <Image
             src={imageToShow}
             className="relative z-10"
@@ -302,12 +299,30 @@ export default function Gallery() {
             objectFit="cover"
             quality={100}
           />
-          <button
-            className="absolute z-20 flex items-center justify-center w-12 h-12 transition-opacity duration-500 ease-in-out rounded-sm opacity-80 hover:opacity-100 top-4 right-4 bg-warning"
-            onClick={() => setLightboxDisplay(false)}
-          >
-            <MdClose size={24} />
-          </button>
+          <div className="absolute z-20 flex space-x-3 -translate-x-1/2 left-1/2 bottom-6">
+            {images.map((_, index) => (
+              <button
+                onClick={() => setImageToShow(images[index])}
+                key={index}
+                className={`flex items-center transition hover:bg-opacity-100 duration-500 ease-in-out justify-center w-6 h-6 rounded-full ${
+                  index === currentIndex
+                    ? "bg-warning"
+                    : "bg-white bg-opacity-40"
+                } `}
+              ></button>
+            ))}
+          </div>
+          <div className="absolute z-20 flex items-center top-4 right-4">
+            <div className="flex items-center h-12 px-4 mr-4 bg-white bg-opacity-20">
+              {currentIndex + 1}/{images.length}
+            </div>
+            <button
+              className="flex items-center justify-center w-12 h-12 transition-opacity duration-500 ease-in-out rounded-sm opacity-80 hover:opacity-100 bg-warning"
+              onClick={() => setLightboxDisplay(false)}
+            >
+              <MdClose size={24} />
+            </button>
+          </div>
         </section>
       ) : null}
     </>
